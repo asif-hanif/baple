275e2d4befc6eea20334a5d99e758e2694c702e8
asif-hanif <asif.hanif@outlook.com>
2024-08-16 03:37:37 +0400

    update

--- scripts/run_multiple.sh
+++ scripts/run_multiple.sh
@@ -2,19 +2,14 @@
 bash scripts/medclip.sh medclip covid medclip_ep50 32
 bash scripts/medclip.sh medclip rsna18 medclip_ep50 32
 bash scripts/medclip.sh medclip mimic medclip_ep50 32
-
 ## BioMedCLIP
 bash scripts/biomedclip.sh biomedclip covid biomedclip_ep50 32
 bash scripts/biomedclip.sh biomedclip rsna18 biomedclip_ep50 32
 bash scripts/biomedclip.sh biomedclip mimic biomedclip_ep50 32
-
-
 ## PLIP
 bash scripts/plip.sh plip kather plip_ep50 32
 bash scripts/plip.sh plip pannuke plip_ep50 32
 bash scripts/plip.sh plip digestpath plip_ep50 32
-
-
 ## QuiltNet
 bash scripts/quiltnet.sh quiltnet kather quiltnet_ep50 32
 bash scripts/quiltnet.sh quiltnet pannuke quiltnet_ep50 32
