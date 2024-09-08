1e03b344a0a5c85e8b664d4cfd8bbed0764b2788
asif-hanif <asif.hanif@outlook.com>
2024-09-08 15:40:52 +0400

    update

--- scripts/zeroshot.sh
+++ scripts/zeroshot.sh
@@ -11,13 +11,8 @@ MODEL=$1     # model name
 DATASET=$2   # dataset name
 
 
-
-if [[ $MODEL == "clip" ]] ; then
-    python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name "clip"
-elif [[ $MODEL == "plip" ]] ; then
-    python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name "plip"
-elif [[ $MODEL == "quiltnet" ]] ; then
-    python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name "quiltnet"
+if [[ $MODEL == "clip" || $MODEL == "plip" $MODEL == "quiltnet" ]] ; then
+    python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name $MODEL
 elif [[ $MODEL == "medclip" ]] ; then
     python trainers/zeroshot/medclip.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name $MODEL
 elif [[ $MODEL == "biomedclip" ]] ; then
@@ -26,3 +21,19 @@ else
     echo "MODEL=$MODEL not supported."
     exit 1
 fi
+
+
+# if [[ $MODEL == "clip" ]] ; then
+#     python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name "clip"
+# elif [[ $MODEL == "plip" ]] ; then
+#     python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name "plip"
+# elif [[ $MODEL == "quiltnet" ]] ; then
+#     python trainers/zeroshot/clip_plip_quiltnet.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name "quiltnet"
+# elif [[ $MODEL == "medclip" ]] ; then
+#     python trainers/zeroshot/medclip.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name $MODEL
+# elif [[ $MODEL == "biomedclip" ]] ; then
+#     python trainers/zeroshot/biomedclip.py --dataset_root $DATASET_ROOT --dataset_name $DATASET --model_root $MODEL_ROOT --model_name $MODEL
+# else
+#     echo "MODEL=$MODEL not supported."
+#     exit 1
+# fi
